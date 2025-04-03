ffe26b8ee694b55f727b932493c0973acdecd2cc
rakeshkolipakaace <rakeshkolipaka@gmail.com>
2025-04-03 21:57:54 +0530

    yaml file

--- solutions/solution.java
+++ solutions/solution.java
@@ -1,41 +1,38 @@
-package solutions;
+package solutions;  // ✅ Make sure your package is named correctly
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public class solution {  // ✅ Renamed to "Solution"
-// java solution
+public class solution {  // ✅ Capitalized class name
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (nums == null || nums.length < 3) return result;
-
+        if (nums == null) return new ArrayList<>();  // ✅ Handle null input
+        
+        int target = 0;
         Arrays.sort(nums);
-        int n = nums.length;
-
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1, right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++;
+        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
                 } else {
-                    right--;
+                    k--;
                 }
             }
         }
-        return result;
+        output.addAll(s);
+        return output;
     }
 }
