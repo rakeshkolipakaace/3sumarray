51178d898e916d2ec8b14bc229f3a806f594bc9d
rakeshkolipakaace <rakeshkolipaka@gmail.com>
2025-04-03 20:30:19 +0530

    cpp

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -1,39 +1,49 @@
-#include <vector>
-#include <algorithm>
-#include <iostream>
-using namespace std;
+// #include <vector>
+// #include <algorithm>
+// #include <iostream>
+// using namespace std;
+
+// class Solution {
+// public:
+// // cpp solution for 3 sum problem from leetcode
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//         vector<vector<int>> result;
+//         sort(nums.begin(), nums.end()); // Sort the array
+
+//         for (int i = 0; i < nums.size() - 2; i++) {
+//             if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicate elements
+
+//             int left = i + 1, right = nums.size() - 1;
+//             while (left < right) {
+//                 int sum = nums[i] + nums[left] + nums[right];
+
+//                 if (sum == 0) {
+//                     result.push_back({nums[i], nums[left], nums[right]});
+//                     left++;
+//                     right--;
+
+//                     // Skip duplicates
+//                     while (left < right && nums[left] == nums[left - 1]) left++;
+//                     while (left < right && nums[right] == nums[right + 1]) right--;
+//                 } 
+//                 else if (sum < 0) {
+//                     left++;
+//                 } 
+//                 else {
+//                     right--;
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// };
+
+
+
 
 class Solution {
-public:
-// cpp solution for 3 sum problem from leetcode
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> result;
-        sort(nums.begin(), nums.end()); // Sort the array
-
-        for (int i = 0; i < nums.size() - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicate elements
-
-            int left = i + 1, right = nums.size() - 1;
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    result.push_back({nums[i], nums[left], nums[right]});
-                    left++;
-                    right--;
-
-                    // Skip duplicates
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                } 
-                else if (sum < 0) {
-                    left++;
-                } 
-                else {
-                    right--;
-                }
-            }
+    public:
+        vector<vector<int>> threeSum(vector<int>& nums) {
+            
         }
-        return result;
-    }
-};
+    };
