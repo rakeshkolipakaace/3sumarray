7e2e855d759f6f83aa47937061bd498009eb25ba
rakeshkolipakaace <rakeshkolipaka@gmail.com>
2025-04-03 22:32:09 +0530

    new commit

--- solutions/solution.py
+++ solutions/solution.py
@@ -1,7 +1,20 @@
-class solution(object):
+class Solution(object):
     def threeSum(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        # write your code here .....
+        if len(nums) < 3:
+            return []
+        nums.sort()
+        res = set()
+        for i, v in enumerate(nums[:-2]):
+            if i >= 1 and v == nums[i-1]:
+                continue
+            d = {}
+            for x in nums[i+1:]:
+                if x not in d:
+                    d[-v-x] = 1
+                else:
+                    res.add((v, -v-x, x))
+        return map(list, res)
