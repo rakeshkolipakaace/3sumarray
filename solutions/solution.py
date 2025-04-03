b9d0014df2b6aece18ddca5ac963045ff3471a67
rakeshkolipakaace <rakeshkolipaka@gmail.com>
2025-04-03 23:31:26 +0530

    yml 1-2

--- solutions/solution.py
+++ solutions/solution.py
@@ -1,38 +1,7 @@
-class Solution:
+class Solution(object):
     def threeSum(self, nums):
-        result = []
-        n = len(nums)
-        
-        if n < 3:
-            return result
-
-        nums.sort()  # Sort the array for two-pointer technique
-
-        for i in range(n - 2):
-            if i > 0 and nums[i] == nums[i - 1]:  # Skip duplicates for 
-                continue
-            
-            left = i + 1
-            right = n - 1
-            target = -nums[i]
-
-            while left < right:
-                sum = nums[left] + nums[right]
-                
-                if sum == target:
-                    result.append([nums[i], nums[left], nums[right]])
-                    
-                    # Skip duplicates for left and right
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    
-                    left += 1
-                    right -= 1
-                elif sum < target:
-                    left += 1
-                else:
-                    right -= 1
-        
-        return result 
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        # write your code here ..
