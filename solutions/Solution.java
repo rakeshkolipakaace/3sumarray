a05d80305d79ec82aec46094d4816d8626086382
rakeshkolipakaace <rakeshkolipaka@gmail.com>
2025-04-04 00:22:26 +0530

    java v

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -3,6 +3,8 @@ package solutions;
 import java.util.*;
 
 public class Solution {  
+
+    //java solution
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         if (nums == null || nums.length < 3) return result;
