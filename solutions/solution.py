de06c99e8530ecd82d1324a6941b5c9b05626365
rakeshkolipakaace <rakeshkolipaka@gmail.com>
2025-04-04 01:35:10 +0530

    py bc 21

--- solutions/solution.py
+++ solutions/solution.py
@@ -1,30 +1,7 @@
-class Solution:
+class Solution(object):
     def threeSum(self, nums):
-        res = []
-        nums.sort()  # Sort the array
-
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]:  # Skip duplicates
-                continue
-
-            left, right = i + 1, len(nums) - 1
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-
-                if total == 0:
-                    res.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-
-                    # Skip duplicate elements
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-
-                elif total < 0:
-                    left += 1
-                else:
-                    right -= 1
-
-        return res
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        # write your code here ...
