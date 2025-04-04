39699fb93ba96e74f0a271a2fb15fc9459446677
rakeshkolipakaace <rakeshkolipaka@gmail.com>
2025-04-04 22:45:54 +0530

    py code bcc

--- solutions/solution.py
+++ solutions/solution.py
@@ -4,18 +4,4 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        if len(nums) < 3:
-            return []
-        nums.sort()
-        res = set()
-        for i, v in enumerate(nums[:-2]):
-            if i >= 1 and v == nums[i-1]:
-                continue
-            d = {}
-            for x in nums[i+1:]:
-                if x not in d:
-                    d[-v-x] = 1
-                else:
-                    res.add((v, -v-x, x))
-        return map(list, res) #my solution
-        
+        # write your code here ....
