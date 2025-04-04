21d96634b77ef04c319dbd657d01ec1f7a777981
rakeshkolipakaace <rakeshkolipaka@gmail.com>
2025-04-04 23:55:23 +0530

    c code

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Solution {  
     public List<List<Integer>> threeSum(int[] nums) {
-        
+        // write your code here
         return new ArrayList<>();
     }
 }
