1fc884e4b9f8c552ac9045787cae5837531169ea
rakeshkolipakaace <rakeshkolipaka@gmail.com>
2025-04-05 12:17:18 +0530

    java ';'

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -4,36 +4,23 @@ import java.util.*;
 
 public class Solution {  
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums); // Sorting helps in using two-pointer approach
-        
+        Set<List<Integer>> result = new HashSet<>();
         int n = nums.length;
+        
         for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates for i
-            
-            int left = i + 1, right = n - 1;
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    
-                    // Skip duplicates for left and right
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                    
-                    left++;
-                    right--;
-                } 
-                else if (sum < 0) {
-                    left++;
-                } 
-                else {
-                    right--;
+            Set<Integer> seen = new HashSet<>();
+            for (int j = i + 1; j < n; j++) {
+                int complement = -nums[i] - nums[j];
+                if (seen.contains(complement)) {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
+                    triplet.sort(null); // Sort to ensure uniqueness
+                    result.add(triplet);
                 }
+                seen.add(nums[j]);
             }
         }
-        return result;
+        return new ArrayList<>(result);
+        
         // write your code here..
        
     }
