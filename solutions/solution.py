ddf3327e83d7c034cd64c6ee3ba1aacae1b8dc58
rakeshkolipakaace <rakeshkolipaka@gmail.com>
2025-04-05 07:09:55 +0530

    'py solutions mng'

--- solutions/solution.py
+++ solutions/solution.py
@@ -4,4 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        # write your code here ....
+        if len(nums) < 3:
+            return []
+        nums.sort()
+        res = set()
+        for i, v in enumerate(nums[:-2]):
+            if i >= 1 and v == nums[i-1]:
+                continue
+            d = {}
+            for x in nums[i+1:]:
+                if x not in d:
+                    d[-v-x] = 1
+                else:
+                    res.add((v, -v-x, x))
+        return map(list, res)
+        
