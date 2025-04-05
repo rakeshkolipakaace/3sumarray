5921eb7d66f7f3510a1f960853f4e8414a7d3761
rakeshkolipakaace <rakeshkolipaka@gmail.com>
2025-04-05 12:13:39 +0530

    java 'sol'

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -4,7 +4,37 @@ import java.util.*;
 
 public class Solution {  
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // Sorting helps in using two-pointer approach
+        
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates for i
+            
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // Skip duplicates for left and right
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                } 
+                else if (sum < 0) {
+                    left++;
+                } 
+                else {
+                    right--;
+                }
+            }
+        }
+        return result;
         // write your code here..
-        return new ArrayList<>();
+       
     }
 }
