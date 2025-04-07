620c5107f89383cb2382caf15aed8aff4ff23627
rakeshkolipakaace <rakeshkolipaka@gmail.com>
2025-04-07 21:47:49 +0530

    java java

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -4,22 +4,8 @@ import java.util.*;
 
 public class Solution {  
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> result = new HashSet<>();
-        int n = nums.length;
-        
-        for (int i = 0; i < n - 2; i++) {
-            Set<Integer> seen = new HashSet<>();
-            for (int j = i + 1; j < n; j++) {
-                int complement = -nums[i] - nums[j];
-                if (seen.contains(complement)) {
-                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
-                    triplet.sort(null); // Sort to ensure uniqueness
-                    result.add(triplet);
-                }
-                seen.add(nums[j]);
-            }
-        }
-        return new ArrayList<>(result);
+       
+        return new ArrayList<>();
         
         // write your code here..
        
