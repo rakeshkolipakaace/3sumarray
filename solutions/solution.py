b327f1718bf3546e7169edc94f054fb16b9627f3
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-29 22:11:50 +0530

    py base code eee

--- solutions/solution.py
+++ solutions/solution.py
@@ -4,27 +4,4 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        nums.sort()
-        result = []
-        
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]:  # Skip duplicate elements
-                continue
-                
-            left, right = i + 1, len(nums) - 1
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total == 0:
-                    result.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-                    while left < right and nums[left] == nums[left - 1]:  # Skip duplicates
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:  # Skip duplicates
-                        right -= 1
-                elif total < 0:
-                    left += 1
-                else:
-                    right -= 1
-        
-        return result
+        # write your code here .....
