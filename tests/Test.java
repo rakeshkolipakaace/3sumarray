8a080f28ef6654046dfb0621ae377bd2a7223c40
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-29 23:39:19 +0530

    java solutionmmmmmm

--- tests/Test.java
+++ tests/Test.java
@@ -72,7 +72,7 @@ import java.util.*;
 
 public class Test {
     static class ThreeSumTest {
-        private Solution solution = new Solution();  
+        private Solution solution = new Solution(); 
 
         private boolean listsEqual(List<List<Integer>> expected, List<List<Integer>> actual) {
             if (expected.size() != actual.size()) return false;
@@ -90,39 +90,24 @@ public class Test {
         }
 
         public void runAllTests() {
-            System.out.println("Test started...");  // Add this line to confirm code execution
+            System.out.println("Test started...");
             runTest("Test 1", new int[]{-1, 0, 1, 2, -1, -4}, 
                     Arrays.asList(Arrays.asList(-1, -1, 2), Arrays.asList(-1, 0, 1)));
-
             runTest("Test 2", new int[]{0, 1, 1}, new ArrayList<>());
-
-            runTest("Test 3", new int[]{0, 0, 0}, 
-                    Arrays.asList(Arrays.asList(0, 0, 0)));
-
-            runTest("Test 4", new int[]{1, 2, -3}, 
-                    Arrays.asList(Arrays.asList(-3, 1, 2)));
-
+            runTest("Test 3", new int[]{0, 0, 0}, Arrays.asList(Arrays.asList(0, 0, 0)));
+            runTest("Test 4", new int[]{1, 2, -3}, Arrays.asList(Arrays.asList(-3, 1, 2)));
             runTest("Test 5", new int[]{1, 1, 1, 1}, new ArrayList<>());
-
             runTest("Test 6", new int[]{-1, -2, -3, -4}, new ArrayList<>());
-
             runTest("Test 7", new int[]{1, 2, 3, 4}, new ArrayList<>());
-
-            runTest("Test 8", new int[]{-105, 0, 105}, 
-                    Arrays.asList(Arrays.asList(-105, 0, 105)));
-
+            runTest("Test 8", new int[]{-105, 0, 105}, Arrays.asList(Arrays.asList(-105, 0, 105)));
             runTest("Test 9", new int[]{}, new ArrayList<>());
-
             runTest("Test 10", null, new ArrayList<>());
-
-            runTest("Test 11", new int[]{0, 0, 0, 0, 0}, 
-                    Arrays.asList(Arrays.asList(0, 0, 0)));
+            runTest("Test 11", new int[]{0, 0, 0, 0, 0}, Arrays.asList(Arrays.asList(0, 0, 0)));
         }
     }
 
     public static void main(String[] args) {
         ThreeSumTest tester = new ThreeSumTest();
-        tester.runAllTests();
+        tester.runAllTests();  // Ensure this method is called to run the tests
     }
 }
-
