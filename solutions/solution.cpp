e89068aeb60a3395e6e7a1071fb5b4d3bd1e06c5
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-29 21:56:33 +0530

    cpp sollllll

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -1,12 +1,41 @@
 #include <vector>
 #include <algorithm>
 
-using namespace std; 
+using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // write your code here..
-      
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end()); // Step 1: Sort the array
+
+        int n = nums.size();
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicate for first number
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    
+                    // Skip duplicates for second and third numbers
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++; // Increase the sum
+                } else {
+                    right--; // Decrease the sum
+                }
+            }
+        }
+
+        return result;
     }
 };
